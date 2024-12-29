3743b765fb5e5ba5792a0e29fd52688d05671e77
gabriel hamouche <ahmed.hamouche@ens.uvsq.fr>
2024-12-29 15:55:18 +0100

    d

---
+++ projet_miage/src/Form/CodeurFilterType.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Form;
+
+use App\Entity\Langage;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CodeurFilterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('langages', EntityType::class, [
+                'class' => Langage::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true, // Pour afficher sous forme de cases à cocher
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // Aucune entité particulière liée
+        ]);
+    }
+}
